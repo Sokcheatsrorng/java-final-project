c949a6900a29664b2188cec2c15cea4bf351df2b
HIENGKOEMHONG <mrkhong70@gmail.com>
2024-06-21 21:33:26 +0700

    Commit third

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/*
+
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/library";
     private static final String USER = "postgres";
@@ -19,3 +21,26 @@ public class DBUtil {
         return connection;
     }
 }
+*/
+
+
+
+public class DBUtil {
+
+    // Replace with your actual JDBC URL, username, and password
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "HONG123";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
+
